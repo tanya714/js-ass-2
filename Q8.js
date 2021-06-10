85e4d5bea66e0dec0700b996d3622443b0524b32
tanya714 <76509126+tanya714@users.noreply.github.com>
2021-06-10 16:30:13 +0530

    Update Q8.js

--- Q8.js
+++ Q8.js
@@ -1,4 +1,38 @@
 //js code to change the background color of the document by using mouseover event of button
 function rollover() {
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = "#95A5A6";
+
+    document.getElementById("add1").style.backgroundColor = "#2E86C1";
+    document.getElementById("add2").style.backgroundColor = "#2E86C1";
+    document.getElementById("add3").style.backgroundColor = "#2E86C1";
+    document.getElementById("add4").style.backgroundColor = "#2E86C1";
+    document.getElementById("add5").style.backgroundColor = "#2E86C1";
+    document.getElementById("add6").style.backgroundColor = "#2E86C1";
+    document.getElementById("add7").style.backgroundColor = "#2E86C1";
+    document.getElementById("add8").style.backgroundColor = "#2E86C1";
+    document.getElementById("add9").style.backgroundColor = "#2E86C1";
+    document.getElementById("add0").style.backgroundColor = "#2E86C1";
+    document.getElementById("add10").style.backgroundColor = "#2E86C1";
+
+    document.getElementById("num").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num1").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num2").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num3").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num4").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num5").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num6").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num7").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num8").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num9").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num0").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num11").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num22").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num33").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num44").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num55").style.backgroundColor = "#D4AC0D";
+    document.getElementById("num66").style.backgroundColor = "#D4AC0D";
+
+    document.getElementById("hr").style.borderTop = "black 4px solid";
+    document.getElementById("sec").style.borderLeft = "black 4px solid";
+
 }
